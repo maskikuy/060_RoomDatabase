e0ec7d7cdbada043798b54b3ecbc59fa3b7ed74e
maskikuy <kproject305@gmail.com>
2025-12-04 12:12:12 +0700

    menambahkan fungs di peta navigasi

--- app/src/main/java/com/example/room_database/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/room_database/view/uicontroller/PetaNavigasi.kt
@@ -4,13 +4,19 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.room_database.view.DetailSiswaScreen
 import com.example.room_database.view.EntrySiswaScreen
 import com.example.room_database.view.HomeScreen
+import com.example.room_database.view.route.DestinasiDetailSiswa
+import com.example.room_database.view.route.DestinasiDetailSiswa.itemIdArg
 import com.example.room_database.view.route.DestinasiEntry
 import com.example.room_database.view.route.DestinasiHome
+import com.example.room_database.viewmodel.DetailSiswa
 
 @Composable
 fun SiswaApp(navController: NavHostController= rememberNavController(), modifier: Modifier){
@@ -28,10 +34,21 @@ fun HostNavigasi(
         composable(DestinasiHome.route){
             HomeScreen(
                 navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                navigateToitemUpdate = {
+                    navController.navigate("${DestinasiDetailSiswa.route}/${it}")}
             )
         }
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = { navController.popBackStack()})
+        }
+        composable (route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(itemIdArg){
+                type = NavType.IntType
+            })
+        ){
+            DetailSiswaScreen(
+                navigateBack = {navController.navigateUp()})
+            )
         }
 
     }
